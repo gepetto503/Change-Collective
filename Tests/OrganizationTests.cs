7da5655d772f3a5ec97aca2f829024250382878a
eluts <eluts15@gmai.com>
2017-06-21 20:20:55 -0700

    add updated database schemas.

--- Tests/OrganizationTests.cs
+++ Tests/OrganizationTests.cs
@@ -27,13 +27,9 @@ namespace GroupProject
     public void Test_Override_ObjectsAreEqual()
     {
       //Arrange, Act
-<<<<<<< HEAD
-      Organization organization1 = new Organization("SaveTheSquirrels", "www.savethesquirrels.org", "savethesquirrels@gmail.com", "Saving the Planet from evil dolphins", "Large bio");
-      Organization organization2 = new Organization("SaveTheSquirrels", "www.savethesquirrels.org", "savethesquirrels@gmail.com", "Saving the Planet from evil dolphins", "Large bio");
-=======
       Organization organization1 = new Organization("SaveTheSquirrels", "www.savethesquirrels.org", "savethesquirrels@gmail.com", "Saving the Planet from evil dolphins", "Large Bio info");
       Organization organization2 = new Organization("SaveTheSquirrels", "www.savethesquirrels.org", "savethesquirrels@gmail.com", "Saving the Planet from evil dolphins", "Large Bio info");
->>>>>>> 2833c32376329253e063591744a7d5d4d00ea2b3
+
       //Assert
       Assert.Equal(organization1, organization2);
     }
@@ -42,11 +38,7 @@ namespace GroupProject
     public void Test_Save_SaveOrganizationToDB()
     {
       //Arrange
-<<<<<<< HEAD
       Organization testOrganization = new Organization("SaveTheDolphins", "www.savethedophins.org", "savethedolpins@gmail.com", "Saving the Planet from squirrel people.", "Large bio info");
-=======
-      Organization testOrganization = new Organization("SaveTheDolphins", "www.savethedophins.org", "savethedolpins@gmail.com", "Saving the Planet from squirrel people.", "Large Bio info");
->>>>>>> 2833c32376329253e063591744a7d5d4d00ea2b3
       testOrganization.Save();
       //Act
       List<Organization> result = Organization.GetAll();
@@ -59,11 +51,7 @@ namespace GroupProject
     public void Test_Find_FindOrganizationInDb()
     {
       //Arrange
-<<<<<<< HEAD
       Organization testOrganization = new Organization("SaveTheDolphins","www.savethedophins.org", "savethedolpins@gmail.com", "Saving the Planet from squirrel people.", "Large bio info");
-=======
-      Organization testOrganization = new Organization("SaveTheDolphins","www.savethedophins.org", "savethedolpins@gmail.com", "Saving the Planet from squirrel people.", "Large Bio info");
->>>>>>> 2833c32376329253e063591744a7d5d4d00ea2b3
       testOrganization.Save();
       //Act
       Organization foundOrganization = Organization.Find(testOrganization.GetId());
@@ -75,11 +63,7 @@ namespace GroupProject
     public void Test_UpdateOrganizationNameInDB()
     {
       //Arrange
-<<<<<<< HEAD
       Organization testOrganizationName = new Organization("SaveTheMoistOwlet", "wwww.savethemoistowlet.com", "savethemoistowlet@gmail.com", "Save the Planet from evil humans who want to spray owlets with water.", "Large bio info");
-=======
-      Organization testOrganizationName = new Organization("SaveTheMoistOwlet", "wwww.savethemoistowlet.com", "savethemoistowlet@gmail.com", "Save the Planet from evil humans who want to spray owlets with water.", "Large Bio info");
->>>>>>> 2833c32376329253e063591744a7d5d4d00ea2b3
       testOrganizationName.Save();
       string updateOrganizationName = "SaveTheBirds";
       //Act
@@ -93,11 +77,7 @@ namespace GroupProject
     public void Test_UpdateOrganizationEmailInDB()
     {
       //Arrange
-<<<<<<< HEAD
       Organization testOrganizationEmail = new Organization("SaveTheMoistOwlet", "wwww.savethemoistowlet.com", "savethemoistowlet@gmail.com", "Save the Planet from evil humans who want to spray owlets with water.", "Large bio info");
-=======
-      Organization testOrganizationEmail = new Organization("SaveTheMoistOwlet", "wwww.savethemoistowlet.com", "savethemoistowlet@gmail.com", "Save the Planet from evil humans who want to spray owlets with water.", "Large Bio info");
->>>>>>> 2833c32376329253e063591744a7d5d4d00ea2b3
       testOrganizationEmail.Save();
       string updateOrganizationEmail = "savethebirds@gmail.com";
       //Act
@@ -111,11 +91,7 @@ namespace GroupProject
     public void Test_UpdateOrganizationBioInDB()
     {
       //Arrange
-<<<<<<< HEAD
       Organization testOrganizationBio = new Organization("SaveTheMoistOwlet", "wwww.savethemoistowlet.com", "savethemoistowlet@gmail.com", "Save the Planet from evil humans who want to burn things:(", "Large bio info");
-=======
-      Organization testOrganizationBio = new Organization("SaveTheMoistOwlet", "wwww.savethemoistowlet.com", "savethemoistowlet@gmail.com", "Save the Planet from evil humans who want to burn things:(", "Large Bio info");
->>>>>>> 2833c32376329253e063591744a7d5d4d00ea2b3
       testOrganizationBio.Save();
       string updateOrganizationBio = "Saving the planet.";
       //Act
@@ -124,8 +100,6 @@ namespace GroupProject
       //Assert
       Assert.Equal(updateOrganizationBio, resultBio);
     }
-
-
 
     public void Dispose()
     {
